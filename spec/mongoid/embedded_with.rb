4e01db863dde9704b466207858faa1c7ef2fa276
Chris Massey <chris@adelyte.com>
2013-05-27 09:54:32 +0800

    Added failing example for attempting to relate a student to a teacher.

--- spec/mongoid/embedded_with.rb
+++ spec/mongoid/embedded_with.rb
@@ -6,5 +6,25 @@ module Mongoid
     it 'should offer the #embedded_with_many macro' do
       Teacher.should respond_to(:embedded_with_many)
     end
+
+    context '#push' do
+      let(:teacher) do
+        teacher = Teacher.new(name: 'Alice')
+        teacher.save
+        teacher
+      end
+
+      let(:student) do
+        student = Student.new(name: 'Bob')
+        student.save
+        student
+      end
+
+      it 'should let a student be pushed to a teacher' do
+        lambda do
+          teacher.students.push student
+        end.should_not raise_error
+      end
+    end # '#push'
   end # EmbeddedWith
 end # Mongoid
